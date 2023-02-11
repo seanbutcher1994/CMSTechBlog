f50c18b8bb2aefecc2b2487b771d981d6068be9a
seanbutcher1994 <110334815+seanbutcher1994@users.noreply.github.com>
2023-02-11 18:30:34 +0800

    a few edits to allow for data seeding

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,29 +1,38 @@
 const sequelize = require('../config/connection');
-const { User, Post, Comment } = require('../models');
+const User = require('../models/User');
+const Post = require('../models/Post');
+const Comment = require('../models/Comment');
 
 const userData = require('./userData.json');
 const postData = require('./postData.json');
 const commentData = require('./commentData.json');
 
 const seedDatabase = async () => {
-    await sequelize.sync({ force: true });
+    try {
+        await sequelize.sync({ force: true });
+    
+        await User.bulkCreate(userData, {
+            individualHooks: true,
+            returning: true,
+        });
+    
+        await Post.bulkCreate(postData, {
+            individualHooks: true,
+            returning: true,
+        });
+    
+        await Comment.bulkCreate(commentData, {
+            individualHooks: true,
+            returning: true,
+        });
+    
+        process.exit(0);
 
-    await User.bulkCreate(userData, {
-        individualHooks: true,
-        returning: true,
-    });
+    } catch(error) {
+        console.error(error);
+        process.exit(1);
+    }
 
-    await Post.bulkCreate(postData, {
-        individualHooks: true,
-        returning: true,
-    });
-
-    await Comment.bulkCreate(commentData, {
-        individualHooks: true,
-        returning: true,
-    });
-
-    process.exit(0);
 };
 
 seedDatabase();
