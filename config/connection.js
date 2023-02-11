f82e4d6d4dd3d1c741833d7a8b0430bf47681405
seanbutcher1994 <110334815+seanbutcher1994@users.noreply.github.com>
2023-02-11 17:29:14 +0800

    get server running and connect to mysql database

---
+++ config/connection.js
@@ -0,0 +1,24 @@
+const Sequelize = require('sequelize');
+const dotenv = require('dotenv');
+dotenv.config();
+
+
+let sequelize
+
+if(process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+    sequelize = new Sequelize(
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PASSWORD,
+        {
+            host: process.env.DB_HOST,
+            dialect:'mysql',
+            port: 3306
+        }
+    );
+
+}
+
+module.exports = sequelize;
