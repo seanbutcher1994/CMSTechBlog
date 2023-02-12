8f6b42ff60c3d21e5575071f6ccc2be1cb9ca5e3
seanbutcher1994 <110334815+seanbutcher1994@users.noreply.github.com>
2023-02-12 15:04:32 +0800

    add route for getting post by ID

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
-const User = require('../models/User');
-const Post = require('../models/Post');
-const Comment = require('../models/Comment');
+// const User = require('../models/User');
+// const Post = require('../models/Post');
+// const Comment = require('../models/Comment');
+
+const { User, Post, Comment } = require('../models')
 
 router.get('/', async (req, res) => {
     try {
@@ -17,6 +19,30 @@ router.get('/', async (req, res) => {
     } catch (error) {
         console.log(error);
         res.status(500).json(error);
+    }
+});
+
+router.get('/post/:id', async (req, res) => {
+    try {
+        const postData = await Post.findByPk(req.params.id, {
+            include: [
+                {
+                    model: User,
+                    attributes: ['name', 'id'],
+                },
+                {
+                    model: Comment,
+                    attributes: ['id', 'text', 'post_id', 'user_id', 'created_at'],
+                    include: {
+                        model: User,
+                        attributes: ['name']
+                    },
+                },
+            ],
+        });
+        res.status(200).json(postData)
+    } catch (error) {
+        res.status(500).json(error)
     }
 })
 
